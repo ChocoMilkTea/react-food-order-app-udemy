b089834950b583c26f18b48de36e724fbee3ac62
ChocoMilkTea <hongjun87@gmail.com>
2021-05-21 22:01:58 +0800

    adding HeaderCartButton separately

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,21 @@
+import CardIcon from '../Cart/CardIcon';
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CardIcon />
+      </span>
+      <span>
+        Your Cart
+      </span>
+      <span className={classes.badge}>
+        3
+      </span>
+
+    </button>
+  );
+}
+
+export default HeaderCartButton;
