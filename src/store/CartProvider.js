c3ffadefa7ee437003fb7abcec5f670368cabffb
ChocoMilkTea <hongjun87@gmail.com>
2021-05-22 22:25:17 +0800

    using context to display cart items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -17,7 +17,7 @@ const CartProvider = (props) => {
   };
 
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
   );
